959c49740adbd15bb580c6def362cbe14c26beb0
Ziggy Zipf <davidarogers@protonmail.com>
2025-12-04 02:20:38 -0800

    modal html

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
+function uiTileCheckmarks() {
+    const tiles = document.getElementById("tile-grid").querySelectorAll("span");
+    tiles.forEach(t => {
+	t.addEventListener("click", () => {
+	    console.log("clicked thing");
+	    t.classList.toggle("checkmark-overlay");
+	    t.classList.toggle("dimmed");
+	});
+    });
+}
+
 function main() {
     console.log("main.js is loaded");
+    uiTileCheckmarks();
 }
 
 window.onload = main;
