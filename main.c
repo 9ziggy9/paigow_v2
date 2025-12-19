f4e960653665ee58d00edde85589fc7354444307
Ziggy Zipf <davidarogers@protonmail.com>
2025-12-19 14:55:19 -0800

    considering tile class only approach, but geejun ranking is consistent

--- main.c
+++ main.c
@@ -15,8 +15,8 @@ HouseWayExceptionList HOUSE_WAY_EXCEPTIONS = {
 					 ((uint16_t)(t2) << 8)  | \
 					 ((uint16_t)(t3) << 4)  | \
 					 ((uint16_t)(t4) << 0))
-#define UNPACK_HAND_INTO_BUFFER(TS, H) do {			       \
-    for (int _N = 0; _N < 4; _N++) (TS)[_N] = GET_NTH_TILE((H), (_N)); \
+#define UNPACK_HAND_INTO_BUFFER(TS, H) do {			                      \
+  for (int _N = 0; _N < 4; _N++) (TS)[_N] = GET_NTH_TILE((H), (_N));  \
 } while(0)
 
 uint16_t sort_hand_by_points(uint16_t hand) {
@@ -25,15 +25,15 @@ uint16_t sort_hand_by_points(uint16_t hand) {
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 3 - i; j++) {
       if (RANK_TO_POINTS[ts[j]] < RANK_TO_POINTS[ts[j + 1]]) {
-	uint8_t tmp = ts[j];
-	ts[j] = ts[j+1];
-	ts[j+1] = tmp;
-	// if equal, we need to additionally sort by pair ranking
+        uint8_t tmp = ts[j];
+        ts[j] = ts[j+1];
+        ts[j+1] = tmp;
+        // if equal, we need to additionally sort by pair ranking
       } else if (RANK_TO_POINTS[ts[j]] == RANK_TO_POINTS[ts[j + 1]]) {
         if (ts[j] < ts[j + 1]) {
-	  uint8_t tmp = ts[j];
-	  ts[j] = ts[j+1];
-	  ts[j+1] = tmp;
+          uint8_t tmp = ts[j];
+          ts[j] = ts[j+1];
+          ts[j+1] = tmp;
         }
       }
     }
@@ -90,8 +90,8 @@ ExceptionMapping map_exception_hand(uint16_t hand) {
     if (hand == HOUSE_WAY_EXCEPTIONS.items[i].in) {
       printf("ENGINE: exception hand detected ... \n");
       return (ExceptionMapping) {
-	.is_exception = true,
-	.hand = HOUSE_WAY_EXCEPTIONS.items[i].out,
+        .is_exception = true,
+        .hand = HOUSE_WAY_EXCEPTIONS.items[i].out,
       };
     }
   }
@@ -113,23 +113,24 @@ void define_exception(HouseWayRule rule) {
   for (size_t i_h1 = 0; i_h1 < h1->num_variants; i_h1++) {
     for (size_t i_h2 = 0; i_h2 < h2->num_variants; i_h2++) {
       for (size_t i_l1 = 0; i_l1 < l1->num_variants; i_l1++) {
-	for (size_t i_l2 = 0; i_l2 < l2->num_variants; i_l2++) {
+        for (size_t i_l2 = 0; i_l2 < l2->num_variants; i_l2++) {
           TileRank ts[4] = {
-	    h1->variants[i_h1],
-	    h2->variants[i_h2],
-	    l1->variants[i_l1],
-	    l2->variants[i_l2]
+            h1->variants[i_h1],
+            h2->variants[i_h2],
+            l1->variants[i_l1],
+            l2->variants[i_l2]
           };
-
-	  uint16_t out = (uint16_t) HAND_FROM_TILES(ts[0], ts[1], ts[2], ts[3]);
-	  uint16_t in  = sort_hand_by_points(out);
+          uint16_t out = (uint16_t) HAND_FROM_TILES
+            (ts[0], ts[1], ts[2], ts[3]);
+          uint16_t in  = sort_hand_by_points(out);
           HouseWayException x = { in, out };
-	  DA_APPEND(HOUSE_WAY_EXCEPTIONS, x);
-	}
+          DA_APPEND(HOUSE_WAY_EXCEPTIONS, x);
+        }
       }
     }
   }
 }
+
 
 void initialize_exceptions(void) {
   printf("ENGINE: initializing exception list ... \n");
@@ -139,20 +140,28 @@ void initialize_exceptions(void) {
   define_exception((HouseWayRule){TC_DAY, TC_CHOPGOW, TC_DAY, TC_FU});
 
   // Gee Jun splits 4-6, 5-6, 6-6
-  define_exception((HouseWayRule){TC_GEEJUN, TC_ANY_SIX, TC_GEEJUN, TC_ANY_FOUR});
+  define_exception((HouseWayRule){
+      TC_GEEJUN, TC_ANY_SIX, TC_GEEJUN, TC_ANY_FOUR
+    });
   define_exception((HouseWayRule){TC_GEEJUN, TC_ANY_SIX, TC_GEEJUN, TC_CHOPNG});
   define_exception((HouseWayRule){TC_GEEJUN, TC_CHONG  , TC_GEEJUN, TC_LOOK});
 
   // Split 9s with any combination of 2, 10, 12 or with non pairing 10s
-  define_exception((HouseWayRule){TC_TEEN_OR_DAY, TC_CHOPGOW, TC_ANY_TEN, TC_CHOPGOW});
+  define_exception((HouseWayRule){
+      TC_TEEN_OR_DAY, TC_CHOPGOW, TC_ANY_TEN, TC_CHOPGOW
+    });
   define_exception((HouseWayRule){TC_TEEN, TC_CHOPGOW, TC_DAY, TC_CHOPGOW});
   define_exception((HouseWayRule){TC_MUUY, TC_CHOPGOW, TC_PING, TC_CHOPGOW});
 
   // Split 8s with any combination of 2, 10, 11, 12, non pairing 10s or 9/11.
   // Need to handle both cases individually.
-  define_exception((HouseWayRule){TC_TEEN_OR_DAY, TC_CHOPBOT, TC_ANY_TEN, TC_CHOPBOT});
+  define_exception((HouseWayRule){
+      TC_TEEN_OR_DAY, TC_CHOPBOT, TC_ANY_TEN, TC_CHOPBOT
+    });
   define_exception((HouseWayRule){TC_TEEN, TC_CHOPBOT, TC_DAY, TC_CHOPBOT});
-  define_exception((HouseWayRule){TC_TEEN_OR_DAY, TC_CHOPBOT, TC_FU, TC_CHOPBOT});
+  define_exception((HouseWayRule){
+      TC_TEEN_OR_DAY, TC_CHOPBOT, TC_FU, TC_CHOPBOT
+    });
   define_exception((HouseWayRule){TC_FU, TC_CHOPBOT, TC_ANY_TEN, TC_CHOPBOT});
   define_exception((HouseWayRule){TC_FU, TC_CHOPBOT, TC_CHOPGOW, TC_CHOPBOT});
   define_exception((HouseWayRule){TC_MUUY, TC_CHOPBOT, TC_PING, TC_CHOPBOT});
@@ -165,11 +174,17 @@ void initialize_exceptions(void) {
 
   // Split 7s with 2/12 and 10/11, handle both cases individually.
   define_exception((HouseWayRule){TC_TEEN, TC_CHIT, TC_DAY, TC_CHIT});
-  define_exception((HouseWayRule){TC_TEEN_OR_DAY, TC_CHIT, TC_ANY_TEN, TC_CHIT});
+  define_exception((HouseWayRule){
+      TC_TEEN_OR_DAY, TC_CHIT, TC_ANY_TEN, TC_CHIT
+    });
   define_exception((HouseWayRule){TC_TEEN_OR_DAY, TC_CHIT, TC_FU, TC_CHIT});
   define_exception((HouseWayRule){TC_TEEN, TC_CHOPCHIT, TC_DAY, TC_CHOPCHIT});
-  define_exception((HouseWayRule){TC_TEEN_OR_DAY, TC_CHOPCHIT, TC_ANY_TEN, TC_CHOPCHIT});
-  define_exception((HouseWayRule){TC_TEEN_OR_DAY, TC_CHOPCHIT, TC_FU, TC_CHOPCHIT});
+  define_exception((HouseWayRule){
+      TC_TEEN_OR_DAY, TC_CHOPCHIT, TC_ANY_TEN, TC_CHOPCHIT
+    });
+  define_exception((HouseWayRule){
+      TC_TEEN_OR_DAY, TC_CHOPCHIT, TC_FU, TC_CHOPCHIT
+    });
 
   
 
@@ -179,69 +194,56 @@ void initialize_exceptions(void) {
 
 typedef struct AnalysisPairs {
   bool has_pairs;
+  bool has_gj;
+  size_t gj_pos;
   size_t num_pairs;
   TileRank tiles[4];
   size_t pair_pos[2];
   uint16_t hand;
 } AnalysisPairs;
 
-// Very interestingly, XOR(2) has property:
-// 0 -> 2, 1 -> 3, 2 -> 0, 3 -> 1
-// see: https://en.wikipedia.org/wiki/Exclusive_or
+void order_hand(AnalysisPairs *info, size_t p1, size_t p2) {
+  info->hand = (uint16_t)
+    HAND_FROM_TILES(info->tiles[p1],   info->tiles[p2],
+                    info->tiles[p1^2], info->tiles[p2^2]);
+}
+
+// See XOR(2) property https://en.wikipedia.org/wiki/Exclusive_or
 AnalysisPairs map_paired_hand(uint16_t hand) {
   AnalysisPairs info = {
-    .has_pairs = false,
-    .num_pairs = 0,
-    .tiles     = {0},
-    .pair_pos  = {0},
-    .hand      = 0,
+    .has_pairs  = false,
+    .tiles      = {0},
+    .pair_pos   = {0},
+    .gj_pos     = 0,
+    .num_pairs  = 0,
+    .hand       = 0,
   };
 
   UNPACK_HAND_INTO_BUFFER(info.tiles, hand);
 
   for (size_t n = 0; n < 3; n++) {
     if (info.tiles[n] == info.tiles[n + 1]) {
+      if (info.tiles[n] == RK_GEEJUN) {
+        info.has_gj = true;
+        info.gj_pos = n;
+      }
       info.has_pairs = true;
       info.pair_pos[info.num_pairs++] = n;
     }
   }
 
-  // Refactor me.
   if (info.has_pairs) {
     size_t p1 = info.pair_pos[0];
     size_t p2 = info.pair_pos[0] + 1;
-    if (info.num_pairs > 1 && info.tiles[p1] < info.tiles[p1^2]) {
-        info.hand = (uint16_t)HAND_FROM_TILES(info.tiles[p1 ^ 2],
-                                              info.tiles[p2 ^ 2],
-                                              info.tiles[p1],
-                                              info.tiles[p2]);
-    } else {
-        info.hand = (uint16_t)HAND_FROM_TILES(info.tiles[p1],
-                                              info.tiles[p2],
-                                              info.tiles[p1 ^ 2],
-                                              info.tiles[p2 ^ 2]);
-    }
-  }
 
-// DEBUG
-#if 1
-  printf("ENGINE: unpacked --> 0x%x 0x%x 0x%x 0x%x\n",
-         info.tiles[0],
-         info.tiles[1],
-         info.tiles[2],
-         info.tiles[3]
-         );
-  if (info.has_pairs) {
-    printf("ENGINE: hand has %zu pairs.\n", info.num_pairs);
-    for (size_t i = 0; i < info.num_pairs; i++) {
-      printf("ENGINE: pair %-8s @ %zu\n",
-             rank_to_name(info.tiles[info.pair_pos[i]]),
-	     info.pair_pos[i]
-             );
-    }
-  }
-#endif
+    if (info.has_gj)
+      order_hand(&info, info.gj_pos, info.gj_pos + 1);
+    else if (info.num_pairs > 1 && info.tiles[p1] < info.tiles[p1 ^ 2])
+      order_hand(&info, p1 ^ 2, p2 ^ 2);
+    else
+      order_hand(&info, p1, p2);
 
+  }
   return info;
 }
 
